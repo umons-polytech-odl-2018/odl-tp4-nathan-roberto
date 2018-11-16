f69f957414e5c623f8109e7d8db66166a0c60b49
Umons-170961 <44571356+Umons-170961@users.noreply.github.com>
2018-11-16 15:02:22 +0100

    erreur....

--- src/main/java/exercise2/Exercise2.java
+++ src/main/java/exercise2/Exercise2.java
@@ -1,6 +1,8 @@
 package exercise2;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,12 +10,28 @@ import java.time.LocalDate;
 
 public class Exercise2 {
 	public static void save(Classroom classroom, Path filePath) {
-
+		try {
+			ObjectOutputStream sortie = new ObjectOutputStream(
+				new FileOutputStream(String.valueOf(filePath)));
+			sortie.writeObject(classroom);
+			sortie.close();
+		}
+		catch(Exception e){}
 	}
 
 	public static Classroom load(Path filePath) {
-		return null;
+		try {
+			ObjectOutputStream sortie = new ObjectOutputStream(
+				new FileOutputStream(String.valueOf(filePath)));
+			return sortie.load(String.valueOf(filePath));
+			sortie.close();
+		}
+		catch(Exception e){}
+		return sortie.load(String.valueOf(filePath));
 	}
+
+
+
 
 	public static void main(String[] args) throws IOException {
 		Teacher teacher = new Teacher("Claire", "Barnett",
